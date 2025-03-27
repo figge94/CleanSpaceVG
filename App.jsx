36fed3122b73f1755aaa9e86d6e4ddca332914cc
Victoria <victoriafingal@hotmail.se>
2025-03-27 23:16:10 +0100

    hmm

--- App.jsx
+++ App.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import {
-  NavigationContainer,
-  DefaultTheme as NavDefaultTheme,
-  DarkTheme as NavDarkTheme
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNavigator";
-import useAppTheme from "./hooks/useAppTheme";
 
 function AppContent() {
-  // Hämta temat från useAppTheme-hooken
-  const { theme } = useAppTheme();
-
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
   );
